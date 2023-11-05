89b130d759623ed1eab121d35697c5495ac1ddc1
Sumanth Dwarakinath <sumanth.dwarakinath@gmail.com>
2023-11-05 22:37:24 +0000

    training data changes

--- week3/create_labeled_queries.py
+++ week3/create_labeled_queries.py
@@ -7,6 +7,14 @@ import csv
 
 # Useful if you want to perform stemming.
 import nltk
+
+
+def stem_query(query):
+    tokens = query.split()
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+    return ' '.join(stemmed_tokens)
+
+
 stemmer = nltk.stem.PorterStemmer()
 
 categories_file_name = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
@@ -44,13 +52,52 @@ for child in root:
         parents.append(cat_path_ids[-2])
 parents_df = pd.DataFrame(list(zip(categories, parents)), columns =['category', 'parent'])
 
+
 # Read the training data into pandas, only keeping queries with non-root categories in our category tree.
 queries_df = pd.read_csv(queries_file_name)[['category', 'query']]
 queries_df = queries_df[queries_df['category'].isin(categories)]
 
 # IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+#lowercase
+queries_df['query'] = queries_df['query'].str.lower()
+#treat anything that is not a number or letter as a space
+queries_df['query'] = queries_df['query'].str.replace('[^a-zA-Z0-9]', ' ', regex=True)
+#trim multiple spaces
+queries_df['query']  = queries_df['query'].str.replace('\s+', ' ', regex=True)
+#use nltk stemmer
+queries_df['query'] = queries_df['query'].apply(stem_query)
+
+# Compute the query counts for all of the categories
+grouped_categories = queries_df.groupby('category').size().reset_index(name='counts')
+# print(grouped_categories.columns)
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+initial_grouped_categories = queries_df.groupby('category').size().reset_index(name='counts')
+print(f'initial_category_count = {initial_grouped_categories.size}')
+
+# IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+while True:
+    # compute grouped categories
+    grouped_categories = queries_df.groupby('category').size().reset_index(name='counts')
+    categories_with_count_lt_min_queries = grouped_categories[grouped_categories['counts'] < min_queries].sort_values(by=['counts'])
+    #print(categories_with_count_lt_1000.size)
+
+    # if there are no categories with count < 1000 then exit
+    if categories_with_count_lt_min_queries.empty:
+        break
+    
+    # find the category with the lowest count 
+    category_with_lowest_count = grouped_categories.loc[categories_with_count_lt_min_queries['counts'].idxmin(), 'category']
+
+    #find the parent of that category
+    parent_category = parents_df[parents_df['category'] == category_with_lowest_count].values[0][1]
+
+    #replace all category with the lowest count with the parent category in queries_df
+    queries_df['category'] = queries_df['category'].replace(category_with_lowest_count, parent_category)
+
+
+final_grouped_categories = queries_df.groupby('category').size().reset_index(name='counts')
+print(f'final_category_count = {final_grouped_categories.size}')
 
 # Create labels in fastText format.
 queries_df['label'] = '__label__' + queries_df['category']
@@ -59,3 +106,4 @@ queries_df['label'] = '__label__' + queries_df['category']
 queries_df = queries_df[queries_df['category'].isin(categories)]
 queries_df['output'] = queries_df['label'] + ' ' + queries_df['query']
 queries_df[['output']].to_csv(output_file_name, header=False, sep='|', escapechar='\\', quoting=csv.QUOTE_NONE, index=False)
+
